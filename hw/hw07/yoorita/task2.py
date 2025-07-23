54cdcbd33f974a33219f9c22b86e76443e43bd69
Rita Lykova <ritaa.lykova@gmail.com>
2025-07-23 13:08:08 +0200

    Added lambda functionality

--- hw/hw07/yoorita/task2.py
+++ hw/hw07/yoorita/task2.py
@@ -2,15 +2,18 @@ PI = 3.14  # rounded and hardcoded PI const
 figures = {
     "1": {
         "name": "Triangle",
-        "elements": ("height", "base")
+        "elements": ["height", "base"],
+        "function": lambda h, b: (h * b) / 2
     },
     "2": {
         "name": "Circle",
-        "elements" :"radius"
+        "elements" : ["radius"],
+        "function": lambda r: PI * r ** 2
     },
     "3": {
         "name": "Rectangle",
-        "elements": ("A side", "B side")
+        "elements": ["A side", "B side"],
+        "function": lambda a, b: a * b
     }
 }
 
@@ -55,15 +58,20 @@ while True:
     figure_details = figures.get(user_input, None)
 
     if figure_details:
-        area = 0
         elements = [validate_input_value(f"Enter the {i}: ") for i in figure_details.get("elements")]
-        match user_input:
-            case "1":
-                area = triangle_area(*elements)
-            case "2":
-                area = circle_area(*elements)
-            case "3":
-                area = rectangle_area(*elements)
+        # Comment this if you don't want to use lambda functions
+        area_function = figure_details.get("function")
+        area = area_function(*elements)
+
+        # !! Uncomment this to use the function call instead of lambda !!
+        # area = 0
+        # match user_input:
+        #     case "1":
+        #         area = triangle_area(*elements)
+        #     case "2":
+        #         area = circle_area(*elements)
+        #     case "3":
+        #         area = rectangle_area(*elements)
 
         print("*" * 10)
         print(f"The area of {figure_details.get("name")} is {area}")
