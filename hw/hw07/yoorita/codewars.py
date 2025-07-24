f96eec929b59b05426ec01661b254a8f2201781d
Rita Lykova <ritaa.lykova@gmail.com>
2025-07-24 14:10:31 +0200

    Solutions for all tasks from Codewars for HW7

--- hw/hw07/yoorita/codewars.py
+++ hw/hw07/yoorita/codewars.py
@@ -32,4 +32,32 @@ def reverse_list(l):
 
 # Multiples of 3 or 5
 def solution(number):
-    pass
+    return sum(i for i in range(0, number) if i % 3 == 0 or i % 5 == 0)
+
+
+# Will you make it?
+def zero_fuel(distance_to_pump, mpg, fuel_left):
+    return distance_to_pump <= mpg * fuel_left
+
+
+# Are You Playing Banjo?
+def are_you_playing_banjo(name):
+    return f"{name} {'plays' if name.lower()[0] == 'r' else 'does not play'} banjo"
+
+
+# Convert boolean values to strings 'Yes' or 'No'.
+def bool_to_word(boolean):
+    return "Yes" if boolean else "No"
+
+
+# Counting sheep...
+def count_sheeps(sheep):
+  return sheep.count(True)
+
+
+# Is this my tail?
+def correct_tail(body, tail):
+    return body[-1] == tail
+
+
+print(solution(5))
