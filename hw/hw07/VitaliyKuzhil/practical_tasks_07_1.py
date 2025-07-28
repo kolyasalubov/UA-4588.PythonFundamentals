a6d86878e3136eef913f14bf85610252a214d209
VitaliyKuzhil <vitaliy.kuzhil2018@gmail.com>
2025-07-28 08:47:23 +0300

    Сompleted task 07.1.2

--- hw/hw07/VitaliyKuzhil/practical_tasks_07_1.py
+++ hw/hw07/VitaliyKuzhil/practical_tasks_07_1.py
@@ -19,3 +19,72 @@ def max_num_of_two(arg_1: int = 0, arg_2:int = 0) -> int | str:
 
 print(f'Possessive arg: {max_num_of_two(num_1, num_2)}')
 print(f'Name arg: {max_num_of_two(arg_1 = num_1, arg_2 = num_2)}')
+
+
+# Task 2
+import math
+
+user_choice = int(input('What do you want to calculate?\n 1 - rectangle, 2 - triangle, 3 - circle? '))
+
+
+def area_of_rectangle(len_reg: float, width_reg: float) -> float:
+    '''
+    Function calculate area of rectangle
+
+    Receive:
+    len_reg: float
+    width_reg: float
+
+    Return: float
+    '''
+    return len_reg * width_reg
+
+
+def area_of_triangle(base_tri:float, height_tri:float) -> float:
+    '''
+    Function calculate area of triangle
+
+    Receive:
+    base_tri: float
+    height_tri: float
+
+    Return: float
+    '''
+    return base_tri * height_tri / 2
+
+
+def area_of_circle(radius_circ: float) -> float:
+    '''
+    Function calculate area of circle
+
+    Receive:
+    radius_circ: float
+
+    Return: float
+    '''
+    return math.pi * math.pow(radius_circ, 2)
+
+
+match user_choice:
+    case 1:
+        len_rectangle = float(input('Enter length of a rectangle: '))
+        width_rectangle = float(input('Enter width of a rectangle: '))
+        
+        rectangle = area_of_rectangle(len_reg = len_rectangle, width_reg = width_rectangle)
+        print(f'The area of the rectangle is: {rectangle}')
+
+    case 2:
+        base_triangle = float(input('Enter base of a triangle: '))
+        height_triangle = float(input('Enter height of a triangle: '))
+
+        triangle = area_of_triangle(base_tri = base_triangle, height_tri = height_triangle)
+        print(f'The area of the triangle is: {triangle}')
+
+    case 3:
+        radius_circle = float(input('Enter radius of a circle: '))
+        
+        circle = area_of_circle(radius_circle)
+        print(f'The area of the circle is: {round(circle, 5)} or {circle / math.pi}')
+
+    case _:
+        print('You must choice one of tre three choices. Try again!')
