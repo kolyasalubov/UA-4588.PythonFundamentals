b2130ab14555041196a6f324c305aa736cb3a729
VitaliyKuzhil <vitaliy.kuzhil2018@gmail.com>
2025-07-29 12:50:16 +0300

    Completed task II. Find The Distance Between Two Points

--- hw/hw07/VitaliyKuzhil/practical_tasks_07_3.py
+++ hw/hw07/VitaliyKuzhil/practical_tasks_07_3.py
@@ -12,3 +12,17 @@ print(greet('Jane'))
 print(greet('Jim'))
 
 print(greet('Johnny'))
+
+
+# ________________ Simple: Find The Distance Between Two Points ___________________
+
+import math
+
+def distance(x1, y1, x2, y2):
+    return round(math.sqrt(math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2)), 2)
+#----------------------------------------------------------------------------------
+
+# distance = lambda x1, y1, x2, y2: round(math.sqrt(math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2)), 2)
+
+#------------------------------- Tests --------------------------------------------
+print(distance(1, 1, 0, 0))
