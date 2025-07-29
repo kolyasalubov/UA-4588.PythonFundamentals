e88e89b885b29bb5eaee7bff80da7b114f43860c
Andrii KOMIAKOV <andrii.komiakov@icloud.com>
2025-07-29 20:47:44 +0300

    finished homework 08

---
+++ hw/hw08/komiakov/08.3/area.py
@@ -0,0 +1,10 @@
+import math
+
+def areaOfCircle(radius):
+    return math.pi * math.pow(radius, 2)
+
+def areaOfRectangle(length, width):
+    return length * width
+
+def areaOfTriangle(base, height):
+    return 0.5 * base * height
