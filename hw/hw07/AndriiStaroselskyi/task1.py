8b65d23a2963faa64f7ee27b6bf4ffc6e4d46ccf
Bambook23 <bambook251@gmail.com>
2025-07-30 14:44:14 +0300

    move solutions for tasks 1-3 into a single file

--- hw/hw07/AndriiStaroselskyi/task1.py
+++
@@ -1,9 +0,0 @@
-def get_largest_number(num_one, num_two):
-    """
-    The function returns the largest of the two given numbers.
-    """
-
-    return num_one if num_one > num_two else num_two
-
-print(get_largest_number(20, 5))
-print(get_largest_number.__doc__)
--- hw/hw07/AndriiStaroselskyi/tasks_1-3.py
+++ hw/hw07/AndriiStaroselskyi/tasks_1-3.py
@@ -1,3 +1,17 @@
+# Task1. Write a function that returns the largest number of two numbers 
+# (use DocStrings documentation strings in the function).
+
+def get_largest_number(num_one, num_two):
+    """
+    The function returns the largest of the two given numbers.
+    """
+
+    return num_one if num_one > num_two else num_two
+
+
+# Task2. Write a program that calculates the area of ​a rectangle, triangle and circle 
+# (write three functions to calculate the area, and call them in the main program depending on the user's choice).
+
 from math import pi
 
 def get_rectangle_area():
@@ -34,3 +48,21 @@ match user_shape_choice:
         print(get_circle_area())
     case _:
         print("Invalid input")
+
+
+# Task3. Write a function that calculates the number of characters included in a given string
+# • input: "hello"
+# • output: {"h":1, "e":1,"l":2,"o":1}
+
+def get_string_chars_number(string: str):
+    """
+    Calculates the number of characters included in a given string
+    """
+    values = {}
+    string = string.lower()
+    for char in string:
+        if char not in values:
+            values[char] = string.count(char)
+    
+    return values
+    
