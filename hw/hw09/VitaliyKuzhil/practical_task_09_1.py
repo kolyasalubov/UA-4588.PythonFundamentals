7d2a0bbf51221b607342d6aff2172faf21ec6053
VitaliyKuzhil <vitaliy.kuzhil2018@gmail.com>
2025-08-08 19:46:12 +0300

    Comleted task 09.1(tkinter) Guess the number

--- hw/hw09/VitaliyKuzhil/practical_task_09_1.py
+++ hw/hw09/VitaliyKuzhil/practical_task_09_1.py
@@ -42,42 +42,43 @@ def input_user_num() -> int:
     
     return user_input
 
+if __name__ == '__main__':
 
-def start_game(attempts: int = 10):
-    '''
-    Function which compare random number with guesses number
-    '''
-    # Get random number
-    rand_num = random_num()
-    # print(rand_num)
+    def start_game(attempts: int = 10):
+        '''
+        Function which compare random number with guesses number
+        '''
+        # Get random number
+        rand_num = random_num()
+        # print(rand_num)
 
-    # Compare random numbers and guesses number
-    while attempts > 0:
-        guess_num = input_user_num()
-        attempts -= 1
+        # Compare random numbers and guesses number
+        while attempts > 0:
+            guess_num = input_user_num()
+            attempts -= 1
+            
+            if guess_num  == rand_num:
+                print('Congratulation!')
+                break
+            elif guess_num < rand_num:
+                print(f'The number is more than {guess_num}. Try again...')
+                continue
+            elif guess_num > rand_num:
+                print(f'The number is less than {guess_num}. Try again...')
+                continue
+        else:
+            print('Your attempts over')
+
+
+    def info_about_game():
+        '''
+        Function in which user get to know rules of game.
+        Auto start the game after rules.
+        '''
+        print('Hi! Let\'s play a game in which you try to guess a number that I chose in the range from 1 to 100.')
         
-        if guess_num  == rand_num:
-            print('Congratulation!')
-            break
-        elif guess_num < rand_num:
-            print(f'The number is more than {guess_num}. Try again...')
-            continue
-        elif guess_num > rand_num:
-            print(f'The number is less than {guess_num}. Try again...')
-            continue
-    else:
-        print('Your attempts over')
+        # Start a game
+        start_game()
 
-
-def info_about_game():
-    '''
-    Function in which user get to know rules of game.
-    Auto start the game after rules.
-    '''
-    print('Hi! Let\'s play a game in which you try to guess a number that I chose in the range from 1 to 100.')
-    
-    # Start a game
-    start_game()
-
-# Run info about game
-info_about_game()
+    # Run info about game
+    info_about_game()
