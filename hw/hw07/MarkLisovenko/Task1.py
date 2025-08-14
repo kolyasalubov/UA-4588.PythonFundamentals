5199277fafdd6902bc04aa30effde9ade89c2bc1
Mark Lisovenko <mmlisovenko@gmail.com>
2025-08-15 00:10:56 +0300

    Added 40th line of the Zen of Python to Zen.txt

--- hw/hw07/MarkLisovenko/Task1.py
+++ hw/hw07/MarkLisovenko/Task1.py
@@ -4,7 +4,11 @@ def largest_number(num1 : int | float = 0, num2 : int | float = 0) :
         return num1
     else : 
         return num2
-    
+
+
+#######################################################################################################################
+
+
 print(largest_number(5, 10)) # Output : 10
 print(largest_number(-7, -49)) # Output : -7
 print(largest_number(-88)) # Output : 0
