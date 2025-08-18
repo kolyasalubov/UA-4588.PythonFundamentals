a454a6a516fe870633383899943291dda86fbc5a
Mark Lisovenko <mmlisovenko@gmail.com>
2025-08-18 15:02:43 +0300

    Completed 07.3 Practical Tasks / [GitHub Outside for Topic 7]

--- hw/hw07/MarkLisovenko/GitHub_Outside_for_Topic_7.py
+++ hw/hw07/MarkLisovenko/GitHub_Outside_for_Topic_7.py
@@ -29,6 +29,61 @@ def number_to_string(num):
     return num
 
 
+# --- Reversing Words in a String ---
+def reverse(st):
+    st = st.split()
+    st.reverse()
+    st = " ".join(st)
+    
+    return st
 
 
+# --- Reverse List Order ---
+def reverse_list(l : list):
+    l.reverse()
+    return l
 
+
+# --- Multiples of 3 or 5 ---
+def solution(number):
+    sum = 0
+    if number < 0 :
+        return 0
+    for i in range(number) :
+        if i % 3 == 0 and i % 5 == 0 or i % 3 == 0 or i % 5 == 0 :
+            sum += i
+    
+    return sum
+
+
+# --- Will you make it? ---
+def zero_fuel(distance_to_pump, mpg, fuel_left):
+    if mpg * fuel_left >= distance_to_pump :
+        return True
+    else :
+        return False
+    
+
+# --- Are You Playing Banjo? ---
+def are_you_playing_banjo(name):
+    if name[0].lower() == "r" :
+        name = name + " plays banjo"
+    else :
+        name = name + " does not play banjo"
+        
+    return name
+
+
+# --- Convert boolean values to strings 'Yes' or 'No’ ---
+def bool_to_word(boolean):
+    return "Yes" if boolean == True else "No"
+
+
+# --- Counting sheep ---
+def count_sheeps(sheep):
+    return sheep.count(True)
+
+
+# --- Is this my tail? ---
+def correct_tail(body, tail):
+    return True if tail == body[-1] else False
